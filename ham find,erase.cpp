c8a68c1ab94c880807426565e3a1cc32fc33ec67
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-09-15 22:21:23 +0700

    Create ham find,erase.cpp

---
+++ ham find,erase.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+    string s2;
+    cin >> s2;
+    cout << s.find(s2); // tra ve vi tri s2 xuat hien trong s
+    // vd: s: 12345
+    // s2: 3
+    // s.find(s2) = 2
+
+    // ham xoa
+    s.erase(s.find(s2),s2.length()+1); // vi tri xoa, do dai xoa bat dau tu vi tri xoa
+}

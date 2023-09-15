fb10c2496573b80d46da64935c7faa42fcde2b7b
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-09-15 21:46:39 +0700

    Create nhập xâu.cpp

---
+++ nhập xâu.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a;
+cin>>a;
+cin.ingore(); // xóa dấu enter khi nhập a;
+string b;
+getline(cin, b); // nhập xâu b có khoảng trống, không ghi lại \0
+b.length() // độ dài xâu
+}

62fa324058a7e81f3a5a5e44beb1ad06daddb7dc
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-09-17 00:38:55 +0700

    Update stringstream.cpp

--- stringstream.cpp
+++ stringstream.cpp
@@ -21,20 +21,39 @@ int main() {
 
     stringstream ss2(data);
     ss2 >> num2;
-    cout << num2 << endl;;
+    cout << num2 << endl; // output 42
     ss2 >> num2;
-    cout << num2;
+    cout << num2; // output 3.14
 
+
+    
+    
     // ghi vao stringstream
     int a = 42;
     int b = 100;
     int c = 999;
     stringstream ss;
-    ss<<a;
+    ss<<a;        // cách này chỉ dùng nếu bạn muốn xuất ra kiểu xâu kí tự
     ss<<b;
     ss<<c;
     string sss= ss.str(); // ham str cho phep truy cap vao du lieu trong stream
     cout << sss; // in ra theo thu tu ghi 42100999
+    
+
+    // nếu muốn ghi từng số riêng biệt như 42 100 999 (int)
+    // thì khi ghi vào stringstream hãy thêm khoảng trống
+    // vì stringstream phân biệt các từ bằng khoảng trống
+    // ví dụ:
+    stringstream ss2;
+    ss<<a<<" ";
+    ss<<b<<" ";         // nó sẽ phân biệt từng số a,b,c
+    ss<<c<<" ";         // ta có thể dễ dàng in ra từng số
+    int num;            // nếu không có khoảng trắng để phân biệt
+    while(ss>>num){     // sẽ gây lỗi khi muốn tương tác với từng giá trị
+        cout << num;
+    }
+
+
 
     
 
@@ -43,12 +62,12 @@ int main() {
     // int=123 -> stringstream -> string=123
     // thay doi tuy thuoc vao kieu du lieu cua c
     string b;
-    getline(cin,b); // nhap 123 kieu string
+    getline(cin,b); // nhap 123 456 kieu string co dau cach phan biet 123 va 456
     stringstream ss(b);
     int c;  // kieu du lieu int
     while (ss >> c)
     {
-        cout << c << endl; // int ra 123 kieu int
+        cout << c << " "; // int ra 123 456 kieu int
     }
 
     return 0;

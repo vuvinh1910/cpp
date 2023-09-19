69e91202c7645ac41b0fdd629e98476668be76d5
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-09-20 00:30:36 +0700

    Update const + auto + new.cpp

--- const + auto + new.cpp
+++ const + auto + new.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+void set(const int &a){ //const là hằng số giúp ta giữ nguyên giá trị a không thay đổi
+  a=100;
+}
+
+void (auto &a){ // hàm auto giúp ta tự động xác định kiểu của a(int)
+  a=100;
+}
+
+int main(){
+    new int a=10;
+    set(a); // thử thay đổi a;
+    cout << a ; // a vẫn 10;
+
+
+    // cấp phát bộ nhớ động
+    int *b=new int; //khai báo con trỏ b để lưu địa chỉ biến động
+    *b=10;          // gán giá trị con trỏ b là 10
+    delete b;       // sau khi xóa thì con trỏ sẽ trở thành con trỏ treo lơ lửng
+}
 

ce9b228118d6e3308788c6ffe5cef4d678a9e57d
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-09-23 01:51:13 +0700

    Update vidu ve nhap xuat va phep cong class.cpp

--- vidu ve nhap xuat va phep cong class.cpp
+++ vidu ve nhap xuat va phep cong class.cpp
@@ -9,19 +9,21 @@ long long gcd(long long a,long long b){  // hàm tìm ucln
 class PhanSo {
 public:
     long long a, b;
-    PhanSo (){};
 
+    // định nghĩa hàm gán giá trị các biến trong class phân số
+    PhanSo (){};
     PhanSo(long long a, long long b){
         this->a = a;
         this->b = b;
     }
 
-    friend istream& operator>>(istream& is, PhanSo& a) {
-        is >> a.a >> a.b;
-        return is;
+    friend istream& operator>>(istream& is, PhanSo& a) {  // hàm nhập, bắt buộc phải là friend của class, tham chiếu giúp ghi lại giá trị
+        is >> a.a >> a.b;				  // istream khai báo is giống như cin
+        return is;					  // trả về hàm nhập
     }
 
-    PhanSo operator+(PhanSo& other){
+    // hàm cộng 2 class phân số, ở đây tương tự là PhanSo + Phanso
+    PhanSo operator+(PhanSo& other){		
         PhanSo result;
         result.a = this->a * other.b + other.a * this->b;
         result.b = this->b * other.b;
@@ -30,10 +32,15 @@ public:
         result.b=result.b/g;
         return result;
     }
+    // hoặc có thể dùng hàm sau gọn hơn khi đã định nghĩa hàm gán biến
+    PhanSo operator+(PhanSo& other){
+        long long u = gcd(this->a * other.b + other.a * this->b, this->b * other.b);
+        return PhanSo((this->a * other.b + other.a * this->b) / u, (this->b * other.b) / u);
+    }
 
-    friend ostream& operator << (ostream& is ,const PhanSo& c){
-        is << c.a << '/' << c.b;
-        return is;
+    friend ostream& operator << (ostream& is ,const PhanSo& c){ // tương tự hàm nhập thì đây là hàm xuất
+        is << c.a << '/' << c.b;				// bạn phải khai báo const PhanSo c trong bài toán này nếu không sẽ bị lỗi
+        return is;  // trả về hàm xuất				// vì đây là cout << p + q nên cần tham chiếu hằng để tránh xung đột vs hàm cộng
     }
 
 };

f8a49c84ab41fbf08c74fe423711cf5ddd7b1fd0
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-09-23 01:53:17 +0700

    Update tim hieu ve class.cpp

--- tim hieu ve class.cpp
+++ tim hieu ve class.cpp
@@ -17,7 +17,7 @@ public: // Phạm vi truy cập public,
     MyClass () {}; //định nghĩa hàm MyClass, nếu không có thì khai báo lớp class sẽ bị lỗi vd: MyClass obj ở main
     MyClass(int a1){  // hàm này khởi tạo đối tượng và gán giá trị biến thành viên vd: MyClass obj(5); MyClass(5);
             this->publicVar = a1;  // this->publicVar ám chỉ giá trị publicVar ở class con này chứ không phải publicVar
-    }                              // ở class lớn trên kia
+    }                              // các class khác
 
 private: // Phạm vi truy cập private,
          // Các thành phần được khai báo trong phạm vi private không thể truy cập từ bên ngoài class và các đối tượng của class

11f9a41db91cac3dd816c6bfb62ebd930dd963d4
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-09-23 15:04:55 +0700

    Update ep kieu ngam, in ra 2 so sau dau phay.cpp

--- ep kieu ngam, in ra 2 so sau dau phay.cpp
+++ ep kieu ngam, in ra 2 so sau dau phay.cpp
@@ -5,7 +5,15 @@ using namespace std;
 int main() {
     int t = 1;
     cout << setfill('0') << setw(5) << t << endl; // in ra 00001
+
+    // hàm setw và setfill có thể áp dụng cho chuỗi string, char
+    string a = "1";
+    cout << setfill('0') << setw(5) << a << endl; // in ra 00001
+
+    
     cout << 1.0*t // ép kiểu ngầm định thành float;
-    cout << fixed << setprecision(2) << t*1.0 << endl;
+        
+    // in ra chữ số sau dấu ,
+    cout << fixed << setprecision(2) << t*1.0 << endl; // in ra 1.00
     return 0;
 }

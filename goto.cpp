97f315a28ee92e69421568e918a2d6d924662864
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-09-27 21:33:42 +0700

    Create goto.cpp

---
+++ goto.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc.h>
+using namespace std;
+int main(){
+  int a;
+  cin >> a;
+  goto case; // nhảy đến case : thực hiện hàm ketthuc và bỏ qua cin >> b;
+ 
+  int b;
+  cin >> b;
+
+  
+  case :
+    void ketthuc();
+    
+  return 0;
+}

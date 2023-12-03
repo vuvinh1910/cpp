6d96ec3ffde12df6ab2b9156c8430a29219dc0bb
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-12-04 00:53:27 +0700

    Update nhap xau,loai bo ky tu xuong dong.cpp

--- nhap xau,loai bo ky tu xuong dong.cpp
+++ nhap xau,loai bo ky tu xuong dong.cpp
@@ -9,5 +9,16 @@ int main(){
         scanf("%[^\n]", s); // không có %*c
         printf("====\nINPUT: %s\n====\n", s);
     }
+    
+    int a;
+    cin >> a;
+    scanf("\n"); // cin.ingore(); neu dung bi loi va nguoc lai, hay bi loi trong class operator>>
+    string b;
+    getline(cin,b);
+
+
+    // luu y
+    scanf("%[^\n]%*c", cc); // neu char cc[] thi se loai bo dc ky tu xuong dong \n
+    scanf("%[^\n]%*c", cc); // se bi loi ko loai bo dc ky tu \n neu la string cc;
 }
 

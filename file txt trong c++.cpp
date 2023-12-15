5a63c14003ab792e93089d60d5bc4f45cbf8887b
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-12-15 22:16:22 +0700

    Update file txt trong c++.cpp

--- file txt trong c++.cpp
+++ file txt trong c++.cpp
@@ -1,31 +1,31 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+
 using namespace std;
 
+int main() {
+    // Mở tệp PTIT.in để đọc
+    ifstream fin("PTIT.in");
+    
+    // Mở tệp PTIT.out để ghi
+    // nếu tệp ko tồn tại thì nó sẽ tự tạo tệp mới.
+    // khi mở tệp thì tất cả dữ liệu của ptit.out đã tồn tại trc đó sẽ bị xóa hết để ghi đè dữ liệu mới.
+    ofstream fout("PTIT.out");
 
-int main (){
-    // Để thao tác với file, trước tiên chúng ta cần phải tạo một con trỏ chỉ đến thực thể của kiểu cấu trúc FILE chứa thông tin của file đó:
-    FILE *file = nullptr;  // gán con trỏ rỗng ban đầu
 
-    // Việc mở file và gán địa chỉ file vào con trỏ thường được tiến hành đồng thời
-    // mở một file PTIT.in bằng hàm fopen() và gán vào con trỏ file:
-    file = fopen("PTIT.in","r");   // fopen(tên file,chế độ mở file), ở đây "r" là chế độ "READ" dùng để đọc file muốn mở
+    // nếu muốn ghi dữ liệu mà ko xóa dữ liệu cũ, hãy mở ptit.out ở chế độ ghi đè.
+    ofstream fout("PTIT.out", ios::app);
+    
 
-    // khai báo con trỏ file2, mở file PTIT.out và gán địa chỉ vào con trỏ file2
-    FILE *file2 = nullptr;
-    file2 = fopen("PTIT.out","w");  // mở file PTIT.out với chế độ "w" là WRITE dùng để ghi vào file
-    // chế độ này sẽ xóa tất cả dữ liệu trong file hiện tại và ghi đè bằng dữ liệu mới.
-
-  
-    char c;  // nên bỏ c = fget(file) trong ngoặc tránh lỗi so sánh
-    while ( (c = fgetc(file)) != EOF)  // Giá trị EOF là một giá trị đặc biệt được sử dụng để chỉ định kết thúc của tệp.
-                                       // Do đó, !=EOF có thể được sử dụng để kiểm tra xem đã đọc đến cuối tệp hay chưa.
-                // hàm fgetc() dùng để đọc tất cả các ký tự trong tệp, khi kết thúc tệp nó sẽ trả về 
-    {
-        fputc(c,file2);  // hàm fputc() dùng để ghi 1 ký tự vào tệp được trỏ đến bởi con trỏ file2  .Cú pháp : fputc(ký tự, file muốn ghi);
+    // Sao chép nội dung từ PTIT.in sang PTIT.out
+    string ch;
+    while (getline(fin,ch)) { // đọc tất cả dòng trong file PTIT.in
+        fout << ch;        // toán tử ghi << và đọc là >>, đừng viết ch >> fout, fout đang ở chế độ ghi dữ liệu.
+		fout << endl;    // hàm getline ko đọc ký tự xuống dòng nên sau khi ghi dòng thì cần phải xuống dòng
     }
 
+    fin.close(); // đóng file.
+    fout.close();
 
-    // mở file thì phải đóng file khi kết thúc, hàm đóng file là : fclose( tệp trỏ đến );
-    fclose(file);
-    fclose(file2);
+    return 0;
 }

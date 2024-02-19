e19c4404afa31144bea0b9ed3b0f0dd31255d051
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-02-19 21:39:55 +0700

    Create luy thua nhanh module.cpp

---
+++ luy thua nhanh module.cpp
@@ -0,0 +1,47 @@
+// Cho số nguyên dương N và K. Hãy tính N^K modulo 10^9+7.
+
+// Input:
+
+// Dòng đầu tiên là số lượng bộ test T (T ≤ 20).
+
+// Mỗi test gồm 1 số nguyên N và K (1 ≤ N ≤ 1000, 1 ≤ K ≤ 10^9).
+
+
+#include <iostream>
+
+using namespace std;
+
+long long pow_mod(long long a, long long b, long long m) {
+    long long result = 1;
+    while (b > 0) {
+        if (b % 2 == 1) {
+            result = (result * a) % m;
+        }
+        a = (a * a) % m;
+        b /= 2;
+    }
+    return result;
+}
+
+// 1.Chuyển đổi K sang hệ nhị phân.
+// 2.Bắt đầu từ bit cao nhất của K, thực hiện các bước sau:
+// a bằng bình phương a mod m.
+// Nếu bit hiện tại là 1, thì nhân kết quả hiện tại với a và mod m.
+// Kết quả sau khi thực hiện xong các bước trên là N^K % m
+
+// ý tưởng:
+    // bình phương và nhân để tính lũy thừa nhanh.
+    // f(k)=(x^y)^2 nếu k=2y;
+    // f(k)=(x^y)^2* nếu k=2y+1;
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        cout << pow_mod(n, k, 1000000007) << endl;
+    }
+    return 0;
+}

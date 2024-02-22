2afe043083c376c8634c54ffa32c079cba670873
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-02-22 19:59:28 +0700

    Create gap doi day so.cpp

---
+++ gap doi day so.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define                     ll long long
+#define faster()            cin.tie(0); ios_base::sync_with_stdio(false); cout.tie(0);
+#define run()               int t; cin >> t; while (t--)
+
+ll Load(ll k, ll x) {
+	if (k % 2 == 1)return x;
+	else return Load(k / 2, x + 1);
+}
+
+int main() {
+	faster();
+	run() {
+		ll n, k; cin >> n >> k;
+		cout << Load(k, 1) << endl;
+	}
+}

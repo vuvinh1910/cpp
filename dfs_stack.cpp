d07352df31631e640631e1833b0faa021e79521d
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-02-25 21:54:17 +0700

    Update dfs_stack.cpp

--- dfs_stack.cpp
+++ dfs_stack.cpp
@@ -8,16 +8,16 @@ void dfs_stack(int u,int check[100],int n,int a[100][100]){
     cout << u << " ";
     while (!st.empty())
     {
-        int s = st.top();  // s luon goi lai moi lan trong vong lap de cap nhat gia tri moi khi stack thay doi.
+        int s = st.top();
         st.pop();
         for (int i = 1; i <= n; i++)
         {
             if(a[s][i]==1 && check[i]==0){
-                st.push(u);
+                cout << i << " ";
+                st.push(s);
                 st.push(i);
                 check[i]=1;
-                dfs_stack(i,check,n,a);
-                break; // break de tranh truong hop 2-4-6 thanh 2-6 bo qua 4;
+                break;
             }
         }
         

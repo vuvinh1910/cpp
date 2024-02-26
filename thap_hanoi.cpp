2896da5ee1a5d89d0020955f16cd4d2950c24384
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-02-26 17:17:16 +0700

    Update thap_hanoi.cpp

--- thap_hanoi.cpp
+++ thap_hanoi.cpp
@@ -5,9 +5,9 @@ void Tower(int n , char a, char b, char c ){
 		cout<<a<<" -> "<<c<<endl;
 		return;
 	}
-	Tower(n-1,a,c,b);
-	Tower(1,a,b,c);
-	Tower(n-1,b,a,c);
+	Tower(n-1,a,c,b); 	// chuyển n-1 đĩa từ a đến cột trung gian b, lấy c làm cột trung gian hiện tại.
+	Tower(1,a,b,c);		// chuyển đĩa lớn nhất còn lại ở cột a đến cột c làm đáy.
+	Tower(n-1,b,a,c);	// chuyển lại n-1 đĩa từ cột b về cột c, lấy cột a làm trung gian hiện tại.
 	}
 int main(){
 	char a='A', b='B', c='C';
